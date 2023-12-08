4634b27a01f0df765606fca30a83ce720635948a
Stuart Himmer <stuarthimmer@loopreturns.com>
2023-12-08 12:54:01 -0700

    part 2

--- sphimmer/aoc_2023/src/day2/solution.ts
+++ sphimmer/aoc_2023/src/day2/solution.ts
@@ -1,7 +1,20 @@
-interface GameSet {
+class GameSet {
     red?: number;
     green?: number;
     blue?: number;
+    constructor(gameRound?: string){
+        if (gameRound) {
+            gameRound.split(', ').map((cubes) => {
+                return getColorNum(cubes);
+            }).map((gs) => {
+                this[gs.color] = gs.count
+            })
+        }
+    }
+
+    getGameSetPower() {
+        return this.red * this.blue * this.green;
+    }
 }
 
 const part1PossibleGameCount = {
@@ -26,8 +39,12 @@ function part1IsGamePossible(gameSet: GameSet): boolean{
 }
 
 function getColorNum(set: string): {color: string, count: number}{
-    const split = set.split(' ');
+    const split = set.trim().split(' ');
     return {color: split[1], count: +split[0]}
+}
+
+function parseGameLine(line: string): RegExpMatchArray {
+    return line.match(/(Game \w+)|(: )|(.*)/g)
 }
 
 export function part1(input: string): number {
@@ -35,25 +52,48 @@ export function part1(input: string): number {
 
     const lines = input.split('\n');
     lines.map((line) => {
-        const gameRegxGroups = line.match(/(Game \w+)|(: )|(.*)/g)
+        const gameRegxGroups = parseGameLine(line)
         const gameId = gameRegxGroups[0].split(' ')[1];
-        const gameSetsString: string = gameRegxGroups[2];
+        const gameRounds: string[] = gameRegxGroups[2].split(';');
 
-        const sets = gameSetsString.split(';').map((segment: string) => {
-            const gameSet: GameSet = {};
-            segment.split(', ').map((cubes) => {
-                cubes = cubes.trim()
-                return getColorNum(cubes);
-            }).map((gs) => {
-                gameSet[gs.color] = gs.count
-            })
-            return gameSet;
-        }).map(set => part1IsGamePossible(set))
+        const sets = gameRounds.map(
+            (round: string) => new GameSet(round)
+        ).map(
+            set => part1IsGamePossible(set)
+        );
         if (!sets.includes(false)) {
             sum += +gameId;
         } 
     })
+    return sum;
+}
 
+function part2GameRoundPower(gameRound: {color: string, count: number}[][]): number {
+    const minimumGameSetPossible: GameSet = new GameSet()
 
+    gameRound.map(sets => {
+        sets.map(set => {
+            if (minimumGameSetPossible[set.color] == undefined) {
+                minimumGameSetPossible[set.color] = set.count;
+            } else if (set.count > minimumGameSetPossible[set.color]) {
+                minimumGameSetPossible[set.color] = set.count;
+            }
+        });
+    })
+    return minimumGameSetPossible.getGameSetPower();
+}
+
+export function part2(input: string): number {
+    const lines = input.split('\n');
+    let sum = 0;
+    lines.map((line) => {
+        const gameRegxGroups = parseGameLine(line)
+        const gameRounds: string[] = gameRegxGroups[2].split(';');
+        const sets = gameRounds.map((round: string) => {
+            return round.split(', ').map(cubes => getColorNum(cubes));
+        });
+        const power = part2GameRoundPower(sets);
+        sum += power;
+    });
     return sum;
 }
