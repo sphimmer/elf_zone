69f4da4100fdf9a4889f5bd0423cd2ac7e6f392c
Stuart Himmer <stuarthimmer@loopreturns.com>
2023-12-08 22:58:11 -0700

    part 2

--- sphimmer/aoc_2023/src/day3/day3.test.ts
+++ sphimmer/aoc_2023/src/day3/day3.test.ts
@@ -1,8 +1,9 @@
 import { input } from "./input";
-import { hasSymbol, part1 } from "./solution"
+import { getAllIndexes, getGearRatioNumbers, hasSymbol, part1, part2 } from "./solution"
 
 
-const testInput = `467..114..
+const testInput = 
+`467..114..
 ...*......
 ..35..633.
 ......#...
@@ -13,10 +14,9 @@ const testInput = `467..114..
 ...$.*....
 .664.598..`;
 
-
+const lines = testInput.split('\n')
 describe("day 3", () => {
     describe("part 1", () => {
-        const lines = testInput.split('\n')
         test('hasSymbol', () => {
             let result = hasSymbol([0,1,2], [lines[0], lines[1]]);
             expect(result).toBe(true)
@@ -40,4 +40,36 @@ describe("day 3", () => {
         })
         
     })
+
+    describe('part 2', () => {
+
+        test('getAllIndexes', () => {
+            let result = getAllIndexes(lines[1].split(''), '*');
+            expect(result).toStrictEqual([3]);
+            const testLine = "....*.....*..";
+            result = getAllIndexes(testLine.split(''), '*');
+            expect(result).toStrictEqual([4,10]);
+        })
+
+        test('getGearRatioNumbers', () => {
+
+            let result = getGearRatioNumbers(3, [lines[1], lines[0], lines[2]]);
+            expect(result).toStrictEqual(['467', '35']);
+
+            result = getGearRatioNumbers(3, [lines[3], lines[4], lines[5]]);
+            expect(result).toStrictEqual(['617']);
+
+            result = getGearRatioNumbers(5, [lines[8], lines[7], lines[9]]);
+            expect(result).toStrictEqual(['755', '598']);
+        })
+        test("part 2 test input", () => {
+            const result = part2(testInput);
+            expect(result).toBe(467835);
+        })
+
+        test("part 2 actual", () => {
+            const result = part2(input);
+            expect(result).toBe(91622824);
+        })
+    });
 })
