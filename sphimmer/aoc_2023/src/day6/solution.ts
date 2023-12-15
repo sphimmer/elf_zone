1d537640914389c3b3c775538286d3ace3a15ef6
Stuart Himmer <stuarthimmer@loopreturns.com>
2023-12-15 14:16:50 -0700

    sphimmer day6 updating part 1 to use part 2 solution that scales better

--- sphimmer/aoc_2023/src/day6/solution.ts
+++ sphimmer/aoc_2023/src/day6/solution.ts
@@ -2,7 +2,7 @@
 export interface Race {
     timeAllowed: number;
     distanceRecord: number;
-    winningTimes: number[];
+    winningTimes: number;
 }
 
 
@@ -12,31 +12,21 @@ export function part1(input: string) {
     const distancs = lines[1].match(/\d+/g).map(time => +time);
     let answer = 1;
     for (let x = 0; x < times.length; x++) {
-        const race = {timeAllowed: times[x], distanceRecord: distancs[x], winningTimes: []}
-        race.winningTimes = p1findWinningTimes(race);
-        answer *= race.winningTimes.length;
+        const race = {timeAllowed: times[x], distanceRecord: distancs[x], winningTimes: 0}
+        race.winningTimes = findWinningTimes(race.timeAllowed, race.distanceRecord);
+        answer *= race.winningTimes;
     }
     return answer;
-}
-
-export function p1findWinningTimes(race: Race) {
-    const winningTimes = []
-    for (let n = 0; n < race.timeAllowed; n++) {
-        if (n*(race.timeAllowed - n) > race.distanceRecord) {
-            winningTimes.push(n)
-        }
-    }
-    return winningTimes
 }
 
 export function part2(input: string) {
     const lines = input.split('\n');
     const time = +lines[0].slice(lines[0].indexOf(":")+1).replaceAll(' ', '');
     const distance = +lines[1].slice(lines[1].indexOf(":")+1).replaceAll(' ', '');
-    return p2findWinningTimes(time, distance);
+    return findWinningTimes(time, distance);
 }
 
-export function p2findWinningTimes(time: number, distance: number) {
+export function findWinningTimes(time: number, distance: number) {
     let rangeStart = 0;
     for (let n = 0; n < time; n++) {
         if(n*(time - n) > distance){
